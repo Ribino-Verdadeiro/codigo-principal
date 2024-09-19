b2cf0410cda08848789617a5abe068911f40a0a8
El Ribino <josefaustoribino05@gmail.com>
2024-09-19 17:22:46 -0300

    Add files via upload

--- baloja.sql
+++ baloja.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 19-Set-2024 às 20:46
+-- Tempo de geração: 19-Set-2024 às 22:22
 -- Versão do servidor: 10.4.6-MariaDB
 -- versão do PHP: 7.3.9
 
@@ -46,7 +46,20 @@ CREATE TABLE `login_clientes` (
 INSERT INTO `login_clientes` (`ID`, `nome`, `email`, `senha`, `is_admin`, `reset_token`, `token_expiration`, `verification_code`) VALUES
 (4, NULL, 'admin@example.com', '$2y$10$cnKkzIo6qC5n3SFSXminZePAgLUS8k7sSpjDqgueb5r3IBEPk8vYa', 2, '1f0496b87a12f890790c0fa80a8ddbf4', '2024-09-15 20:11:02', '575221'),
 (5, 'João Pedro Diniz Nacur', 'luisoujpof@gmail.com', '$2y$10$CMAN.fVK25o7OguNt7Wr9OfJ2BXEePxd7zNkDpY3dds8l0tLCBrX2', 0, NULL, NULL, '449467'),
-(6, 'Gabriel ', '7stevensdiamante@gmail.com', '$2y$10$YOGlGSHrtO8zHgGMjhjFEu0qoqfWTBnTg.IZpGDahuclw9FN6Oi1S', 0, NULL, NULL, 'e7f00bf653ab');
+(6, 'Gabriel ', '7stevensdiamante@gmail.com', '$2y$10$YOGlGSHrtO8zHgGMjhjFEu0qoqfWTBnTg.IZpGDahuclw9FN6Oi1S', 0, NULL, NULL, 'e7f00bf653ab'),
+(7, 'Monique', 'monique@mail.com', '$2y$10$rFRRK8es2IdQXQ88RiNhH.GPw7/ooh9qm3ERc5j6Zg4R26tElKi5a', 0, NULL, NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `pedidos`
+--
+
+CREATE TABLE `pedidos` (
+  `id` int(11) NOT NULL,
+  `data_pedido` date DEFAULT NULL,
+  `total` float DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -76,7 +89,7 @@ CREATE TABLE `produtos` (
 --
 
 INSERT INTO `produtos` (`id`, `nome`, `descricao`, `preco`, `altura`, `largura`, `comprimento`, `foto`, `foto2`, `foto3`, `foto4`, `foto5`, `foto6`, `categoria`) VALUES
-(11, 'Lancheira retangular', 'Mantenha seus lanches frescos e organizados com nossa lancheira retangular personalizada. Feita com materiais de alta qualidade, ela possui um design compacto e espaÃ§oso, ideal para o dia a dia. Com forro tÃ©rmico, alÃ§a ajustÃ¡vel e opÃ§Ãµes de personalizaÃ§Ã£o com nome ou estampas, Ã© perfeita para levar estilo e praticidade em qualquer lugar.', '85.00', '20.00', '15.00', '25.00', '0dab1a4e-c941-465c-bc5b-630a81507f8d.jpg', '3a98f714-edf1-4056-9517-e01ccbe5ad9c.jpg', '5f732748-289f-43d8-af55-be93345b9b65.jpg', '991f3884-295b-49ea-9c07-43ee451bf0d4.jpg', NULL, NULL, 'Lancheira');
+(11, 'Lancheira', 'Mantenha seus lanches frescos e organizados com nossa lancheira retangular personalizada. Feita com materiais de alta qualidade, ela possui um design compacto e espaÃ§oso, ideal para o dia a dia. Com forro tÃ©rmico, alÃ§a ajustÃ¡vel e opÃ§Ãµes de personalizaÃ§Ã£o com nome ou estampas, Ã© perfeita para levar estilo e praticidade em qualquer lugar.', '85.00', '20.00', '15.00', '25.00', '0dab1a4e-c941-465c-bc5b-630a81507f8d.jpg', '3a98f714-edf1-4056-9517-e01ccbe5ad9c.jpg', '5f732748-289f-43d8-af55-be93345b9b65.jpg', '991f3884-295b-49ea-9c07-43ee451bf0d4.jpg', NULL, NULL, 'Lancheira');
 
 --
 -- Índices para tabelas despejadas
@@ -87,6 +100,12 @@ INSERT INTO `produtos` (`id`, `nome`, `descricao`, `preco`, `altura`, `largura`,
 --
 ALTER TABLE `login_clientes`
   ADD PRIMARY KEY (`ID`);
+
+--
+-- Índices para tabela `pedidos`
+--
+ALTER TABLE `pedidos`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Índices para tabela `produtos`
@@ -102,7 +121,13 @@ ALTER TABLE `produtos`
 -- AUTO_INCREMENT de tabela `login_clientes`
 --
 ALTER TABLE `login_clientes`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT de tabela `pedidos`
+--
+ALTER TABLE `pedidos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de tabela `produtos`
